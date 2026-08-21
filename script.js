35e8c1dc3535722fccdfcd0858f010a67b827919
artyan14 <lamcot2003@gmail.com>
2026-08-21 23:12:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+    const timer = document.getElementById("timer");
+    const raceDate = new Date("2026-08-23T18:00:00");
+    function updateTimer() {
+        const now = new Date();
+        const difference = raceDate - now;
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+        const hours = Math.floor(
+        (difference / (1000 * 60 * 60)) % 24
+        );
+
+        const minutes = Math.floor(
+            (difference / (1000 * 60)) % 60
+        );
+
+        const seconds = Math.floor(
+            (difference / 1000) % 60
+        );
+        document.getElementById("timer").textContent =
+        `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        }
+
+        updateTimer();
+        setInterval(updateTimer, 1000);
